0d1f7db7b0c03597f0d96b07e8ec10d49945e240
walid salmi <salamiwalid1999@gmail.com>
2023-10-04 03:09:03 -0700

    tasks

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-# This script matched string with h followed by 0 or
-# 1 occurence of b then tn
-
 puts ARGV[0].scan(/hb?tn/).join
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-# This script matches string with hb followed
-# by one or more occurences of t then n
-
 puts ARGV[0].scan(/hbt+n/).join
